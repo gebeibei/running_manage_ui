37b73b208dfec099d1cd75cd9ae3d39b5f866d3a
gebb <gebb@pukkasoft.cn>
2024-12-26 17:34:24 +0800

    feat: add some fns;

--- src/common/composables/useRun.ts
+++ src/common/composables/useRun.ts
@@ -1,7 +1,7 @@
 /* eslint-disable unused-imports/no-unused-vars */
 import data from "@/data/activities.json"
-import { RUN_TITLES } from "@@/constants/const"
 import { cloneDeep } from "lodash-es"
+import { formatPace, formatRunTime, titleForRun } from "../utils/utils"
 
 export type Coordinate = [number, number]
 
@@ -50,58 +50,6 @@ export interface ListConfig {
     address?: string
     runTime?: string
 };
-
-const formatPace = (d: number) => {
-    if (Number.isNaN(d)) return "0"
-    const pace = (1000.0 / 60.0) * (1.0 / d)
-    const minutes = Math.floor(pace)
-    const seconds = Math.floor((pace - minutes) * 60.0)
-    return `${minutes}'${seconds.toFixed(0).toString().padStart(2, "0")}"`
-}
-
-const convertMovingTime2Sec = (moving_time: string) => {
-    if (!moving_time) return 0
-    const splits = moving_time.split(", ")
-    const days = splits.length === 2 ? Number.parseInt(splits[0]) : 0
-    const time = splits.splice(-1)[0]
-    const [hours, minutes, seconds] = time.split(":").map(Number)
-    const totalSeconds = ((days * 24 + hours) * 60 + minutes) * 60 + seconds
-    return totalSeconds
-}
-
-const formatRunTime = (moving_time: string) => {
-    const totalSeconds = convertMovingTime2Sec(moving_time)
-    const seconds = totalSeconds % 60
-    const minutes = (totalSeconds - seconds) / 60
-    if (minutes === 0) {
-        return `${seconds}s`
-    }
-    return `${minutes}min`
-}
-
-const titleForRun = (run: Activity) => {
-    const runDistance = run.distance / 1000
-    const runHour = +run.start_date_local.slice(11, 13)
-    if (runDistance > 20 && runDistance < 40) {
-        return RUN_TITLES.HALF_MARATHON_RUN_TITLE
-    }
-    if (runDistance >= 40) {
-        return RUN_TITLES.FULL_MARATHON_RUN_TITLE
-    }
-    if (runHour >= 0 && runHour <= 10) {
-        return RUN_TITLES.MORNING_RUN_TITLE
-    }
-    if (runHour > 10 && runHour <= 14) {
-        return RUN_TITLES.MIDDAY_RUN_TITLE
-    }
-    if (runHour > 14 && runHour <= 18) {
-        return RUN_TITLES.AFTERNOON_RUN_TITLE
-    }
-    if (runHour > 18 && runHour <= 21) {
-        return RUN_TITLES.EVENING_RUN_TITLE
-    }
-    return RUN_TITLES.NIGHT_RUN_TITLE
-}
 
 /**
  * 跑步记录数据
